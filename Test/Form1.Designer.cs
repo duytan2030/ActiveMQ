08f0dae9ff6346682d751862c7797eba11e8ad9e
Student <Student@H72M19>
2020-05-15 19:58:22 +0700

    Add project files.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_send = new System.Windows.Forms.TextBox();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_send
+            // 
+            this.txt_send.Location = new System.Drawing.Point(26, 24);
+            this.txt_send.Name = "txt_send";
+            this.txt_send.Size = new System.Drawing.Size(100, 20);
+            this.txt_send.TabIndex = 0;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(141, 21);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "Send";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.txt_send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_send;
+        private System.Windows.Forms.Button btn_OK;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.Util;
+using BusinessObject;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+                  
+
+            IConnectionFactory factory = new NMSConnectionFactory("tcp://localhost:61616");
+            IConnection connect = factory.CreateConnection();
+            connect.Start();
+            ISession session = connect.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            ActiveMQQueue ac = new ActiveMQQueue("Tan ne");
+            IMessageProducer MessPorocedure = session.CreateProducer(ac);
+            //IMessage mess = new ActiveMQTextMessage(txt_send.Text);
+
+
+            //MessPorocedure.Send(mess);
+            MessPorocedure.Send(session.CreateTextMessage(txt_send.Text));
+            session.Close();
+            connect.Stop();
+        }
+    }
+}
